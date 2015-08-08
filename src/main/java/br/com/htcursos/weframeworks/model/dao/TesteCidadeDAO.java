f4b5cc6b4824ebac0df8a85217c96fd8fa9b9af8
Higor Nucci <higornucci@gmail.com>
2015-08-08 15:56:33 -0400

    Finalizando aula 2

--- src/main/java/br/com/htcursos/weframeworks/model/dao/TesteCidadeDAO.java
+++ src/main/java/br/com/htcursos/weframeworks/model/dao/TesteCidadeDAO.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import br.com.htcursos.weframeworks.model.entidade.Cidade;
 import br.com.htcursos.weframeworks.model.entidade.Estado;
+import br.com.htcursos.weframeworks.model.entidade.EstadoDAO;
 
 public class TesteCidadeDAO {
 	public static void main(String[] args) {
@@ -11,10 +12,12 @@ public class TesteCidadeDAO {
 				ClassPathXmlApplicationContext(
 						"file:src/main/webapp/WEB-INF/springbeans.xml");
 		CidadeDAO cidadeDAO = (CidadeDAO) ctx.getBean("cidadeDAO");
+		EstadoDAO estadoDAO = (EstadoDAO) ctx.getBean("estadoDAO");
 		
 		Estado ms = new Estado("Mato Grosso do Sul", "MS");
 		Cidade campoGrande = new Cidade("Campo Grande", ms);
 		
+		estadoDAO.salvar(ms);
 		cidadeDAO.salvar(campoGrande);
 		cidadeDAO.buscarPorId(campoGrande);
 		System.out.println(campoGrande.getEstado().getNome());
