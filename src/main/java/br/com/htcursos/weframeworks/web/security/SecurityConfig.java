5ce80c88a99c9f95290c6214def449e048f1ef45
Higor Nucci <higornucci@gmail.com>
2015-09-18 23:52:45 -0400

    spring security

---
+++ src/main/java/br/com/htcursos/weframeworks/web/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package br.com.htcursos.weframeworks.web.security;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("org.postgresql.Driver");
+        ds.setUrl("jdbc:postgresql://localhost:5432/webframeworks");
+        ds.setUsername("postgres");
+        ds.setPassword("150754");
+		
+        auth.jdbcAuthentication().dataSource(ds)
+		.usersByUsernameQuery("select login, senha, ativo from usuario where login=?")
+		.authoritiesByUsernameQuery("select u.login, p.descricao from usuario u, perfil p where u.perfil_id=p.id and login=?");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/admin/**").access("authenticated")  
+		.and()
+			.formLogin().loginPage("/login.xhtml")
+			.usernameParameter("login").passwordParameter("senha")
+			.defaultSuccessUrl("/admin/inserirusuario.xhtml")
+		.and()
+			.logout().logoutSuccessUrl("/login.xhtml")
+		.and()
+			.exceptionHandling();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+}
---
+++ src/main/java/br/com/htcursos/weframeworks/web/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package br.com.htcursos.weframeworks.web.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer /*extends AbstractSecurityWebApplicationInitializer*/ {
+//	public SecurityWebApplicationInitializer() {  
+//        super(SecurityConfig.class);  
+//    } 
+}
